fcadbcf7ea14867064c21e02cf90c8f8f95b6dec
Shubh A Chudasama <shubh.cs@proton.me>
2023-03-20 19:35:30 +0530

    Make front-end (part 2) work with server (part 3)
    For this append the api url with /api in the .env.local file (part 2).
    
    3.9 phonebook backend step9

--- part3/phonebook/index.ts
+++ part3/phonebook/index.ts
@@ -1,8 +1,10 @@
+import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import { PersonSchema, type Person } from "./types";
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(
