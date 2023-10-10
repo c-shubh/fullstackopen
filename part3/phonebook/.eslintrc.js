560f092062bcddea68abfa1fb350a47110372e82
Shubh A Chudasama <shubh.cs@proton.me>
2023-10-11 00:42:11 +0530

    Remove `linebreak-style` eslint rule

--- part3/phonebook/.eslintrc.js
+++ part3/phonebook/.eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   rules: {
     indent: ["error", 2, { SwitchCase: 1 }],
-    "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
     eqeqeq: "error",
