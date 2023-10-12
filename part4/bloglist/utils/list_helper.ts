6d9c1dc57242b0fe61024d9a2a49ba882ace72dd
Shubh A Chudasama <shubh.cs@proton.me>
2023-10-12 20:02:21 +0530

    Add tests for favoriteBlog function
    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.ts
+++ part4/bloglist/utils/list_helper.ts
@@ -11,7 +11,18 @@ const totalLikes = (blogs: BlogT[]) => {
   return sum;
 };
 
+const favoriteBlog = (blogs: BlogT[]): BlogT | undefined => {
+  let favIdx = 0;
+  for (let i = 1; i < blogs.length; i++) {
+    if (blogs[i].likes > blogs[favIdx].likes) {
+      favIdx = i;
+    }
+  }
+  return blogs[favIdx];
+};
+
 export default {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
