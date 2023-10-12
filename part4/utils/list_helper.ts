4a582dd511f27ccd23824393a6d661dc09f4132f
Shubh Chudasama <shubh.cs@proton.me>
2023-10-12 09:40:23 +0000

    Add tests for `totalLikes` function
    4.4: helper functions and unit tests, step2

--- part4/utils/list_helper.ts
+++ part4/utils/list_helper.ts
@@ -5,6 +5,13 @@ const dummy = (blogs: BlogT[]) => {
   return 1;
 };
 
+const totalLikes = (blogs: BlogT[]) => {
+  let sum = 0;
+  for (const blog of blogs) sum += blog.likes;
+  return sum;
+};
+
 export default {
   dummy,
+  totalLikes,
 };
